58db4b8fc68e8b49060280d730bda6269c548fa8
adrian1230 <adrian1145d98@gmail.com>
2020-10-29 02:23:24 +0800

    adrian

--- IFBooth_ios/ContentView.swift
+++ IFBooth_ios/ContentView.swift
@@ -37,14 +37,14 @@ struct Home : View {
     var body: some View {
         VStack {
             Image("jojo")
-            Text("fuck off")
+            Text("Come Get It")
                 .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                 .fontWeight(.bold)
-                .foregroundColor(self.color)
+                .foregroundColor(Color("Color2"))
                 .padding(.top,35)
             TextField("Email",text:self.$email)
                 .padding()
-                .background(RoundedRectangle(cornerRadius: 4).stroke(self.email != "" ? Color("color"):self.color,lineWidth: 2))
+                .background(RoundedRectangle(cornerRadius: 4).stroke(self.email != "" ? Color("Color"):self.color,lineWidth: 2))
                 .padding(.top,25)
             HStack(spacing:15) {
                 VStack{
@@ -56,12 +56,41 @@ struct Home : View {
                     }
                 }
                 Button(action: {
-                    
+                    self.visible.toggle()
                 }) {
                     Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
                         .foregroundColor(self.color)
                 }
             }
+            .padding()
+            .background(RoundedRectangle(cornerRadius: 4).stroke(self.pass != "" ? Color("Color"):self.color,lineWidth: 2))
+            .padding(.top,25)
+            
+            HStack {
+                Spacer()
+                
+                Button(action: {
+                    
+                }) {
+                    Text("Forget password")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("Color"))
+                }
+            }
+            .padding(.top,25)
+            
+            Button(action: {
+                
+            }) {
+                Text("LogIn")
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding(.vertical)
+                    .frame(width: UIScreen.main.bounds.width - 50)
+            }
+            .background(Color("Color2"))
+            .cornerRadius(10)
+            .padding(.top,20)
         }
         .padding(.horizontal,25)
     }
